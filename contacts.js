510a64b4f78651268fc5315833c18f407546e671
Bohdana1958 <139041084+Bohdana1958@users.noreply.github.com>
2024-03-27 16:12:34 +0200

    Remove

--- contacts.js
+++ contacts.js
@@ -26,15 +26,17 @@ async function removeContact(contactId) {
   try {
     const data = await fs.readFile(contactsPath, "utf-8");
     const contacts = JSON.parse(data);
-    const removeContacts = contacts.find((contact) => contactId === contactId);
-    if (!removeContacts) {
+    const contactToRemove = contacts.find(
+      (contact) => contact.id === contactId
+    );
+    if (!contactToRemove) {
       return null;
     }
     const updatedContacts = contacts.filter(
-      (contact) => contact.id !== removeContacts.id
+      (contact) => contact.id !== contactToRemove.id
     );
     await fs.writeFile(contactsPath, JSON.stringify(updatedContacts));
-    return removeContacts;
+    return contactToRemove;
   } catch (error) {
     console.log(error);
   }
